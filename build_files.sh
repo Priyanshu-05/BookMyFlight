f0b2023ca05b81edaad1395292521793df57782c
Priyanshu-05 <dev.priyanshuranjan@gmail.com>
2024-07-13 14:18:02 +0530

    more changes

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# Install Python dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
